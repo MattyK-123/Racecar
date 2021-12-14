4d9e450dffdf9e271cdcd50ffbdde6d4af8b5af8
Matthew <mattkelly048@gmail.com>
2021-12-14 08:51:02 +0200

    Added timing functionality

--- Source/Racecar.py
+++ Source/Racecar.py
@@ -1,6 +1,10 @@
-class Spaceship:
+class Racecar:
+    # Initialize the cars starting location.
     def __init__(self):
         # Racecar coordinates and rotation angle.
         self.x = 0
         self.y = 0
         self.a = 0
+
+    def update(self):
+        pass
--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def main():
     # Create game window.
     screen = pygame.display.set_mode(size)
 
+    # Create new pygame clock object to maintain constant frame-rate.
+    clock = pygame.time.Clock()
+
     # Define a variable to control the main loop.
     running = True
 
     # Start the main gameplay loop.
     while running:
-
+        # Caps the frame-rate to 60 and returns the change in time in seconds.
+        dt = clock.tick(60) * 10 ** (-3)
 
         # Terminates the application when the user clicks the exit button.
         for event in pygame.event.get():
